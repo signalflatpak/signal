aa2ffbc144f535afa6bfa4106278f0b132a1105b
Adam <me@adamthiede.com>
2025-12-13 10:07:10 -0600

    clean up ci-build (#4)
    * clean up ci-build
    
    * further cleanup of autobuild and ci-build
    
    * fix node version sedding
    
    * rename autobuild workflow
    
    * add arch opt :\
    
    * put them all in one line
    
    * remove sleep
    
    * don't stop or rm containers
    
    * cleanup, compact
    
    * remove desktop file while we're at it
    
    * readme update

--- ci-build.sh
+++ ci-build.sh
@@ -1,45 +1,64 @@
-#!/bin/bash
-echo "###ci-build.sh###"
-set -x
+#!/usr/bin/env bash
 
-if [ "$1" == "amd64" ]; then
+while getopts "a:n:v:b:" flag; do
+    case "${flag}" in
+        a) export ARCH=$OPTARG;;
+        n) export NODE_VERSION=$OPTARG;;
+	v) export VERSION=$OPTARG ;;
+        b) export BRANCH=$OPTARG ;;
+    esac
+done
+
+echo $VERSION $BRANCH $NODE_VERSION
+
+if [ "$ARCH" == "amd64" ]; then
 	ARCHSPECIFICVARIABLECOMMON="amd64"
 	ARCHSPECIFICVARIABLESHORT="x64"
-fi
-
-if [ "$1" == "arm64" ]; then
+elif [ "$ARCH" == "arm64" ]; then
 	ARCHSPECIFICVARIABLECOMMON="arm64"
 	ARCHSPECIFICVARIABLESHORT="arm64"
+else
+	echo "arch not set properly; exiting"
+	exit 1
 fi
 
-NODE_VERSION=v22.21.1
+set -x
 
 shopt -s localvar_inherit
 podman create --name=signal-desktop-"$VERSION" --arch "$ARCHSPECIFICVARIABLECOMMON" -it ghcr.io/flatpaks/signalimage:latest bash
-#podman create --name=signal-desktop-"$VERSION" --arch "$ARCHSPECIFICVARIABLECOMMON" -it debian:bookworm bash
 podman start signal-desktop-"$VERSION"
-podman exec -it --env="PATH=/opt/node/bin:$PATH" signal-desktop-"$VERSION" apt -qq update
-#podman exec -it --env="PATH=/opt/node/bin:$PATH" signal-desktop-"$VERSION" apt -qq install -y python3 gcc g++ make build-essential git git-lfs libffi-dev libssl-dev libglib2.0-0 libnss3 libatk1.0-0 libatk-bridge2.0-0 libx11-xcb1 libgdk-pixbuf-2.0-0 libgtk-3-0 libdrm2 libgbm1 ruby ruby-dev curl wget clang llvm lld clang-tools generate-ninja ninja-build pkg-config tcl wget libpixman-1-dev libcairo2-dev libpango1.0-dev
-podman exec -it --env="PATH=/opt/node/bin:$PATH" signal-desktop-"$VERSION" git clone -q https://github.com/signalapp/Signal-Desktop -b 7.82.x
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /opt/ signal-desktop-"$VERSION" wget -q https://nodejs.org/dist/"$NODE_VERSION"/node-"$NODE_VERSION"-linux-"$ARCHSPECIFICVARIABLESHORT".tar.gz
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /opt/ signal-desktop-"$VERSION" tar xf node-"$NODE_VERSION"-linux-"$ARCHSPECIFICVARIABLESHORT".tar.gz
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /opt/ signal-desktop-"$VERSION"  mv node-"$NODE_VERSION"-linux-"$ARCHSPECIFICVARIABLESHORT" node
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /opt/ signal-desktop-"$VERSION" gem install fpm
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" git-lfs install
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" git config --global user.name name
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" git config --global user.email name@example.com
-# The mock tests are broken on custom arm builds
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" sed -r '/mock/d' -i package.json
-# Dry run
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" npm install -g pnpm
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" npm install -g cross-env
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" npm install -g npm-run-all
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" pnpm install
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" rm -rf ts/test-mock
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" pnpm run generate
-# sticker creator
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop/sticker-creator signal-desktop-"$VERSION" pnpm install
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop/sticker-creator signal-desktop-"$VERSION" pnpm run build
-podman exec -it --env="PATH=/opt/node/bin:$PATH" -w /Signal-Desktop signal-desktop-"$VERSION" pnpm run build:release --"$ARCHSPECIFICVARIABLESHORT" --linux
 
+function podman_exec() {
+    dir=$1
+    shift 1
+    podman exec -it -w $dir signal-desktop-"$VERSION" $@
+}
+
+podman_exec / git clone -q https://github.com/signalapp/Signal-Desktop -b $BRANCH
+
+podman_exec /opt/ wget -q https://nodejs.org/dist/"$NODE_VERSION"/node-"$NODE_VERSION"-linux-"$ARCHSPECIFICVARIABLESHORT".tar.gz
+podman_exec /opt/ tar xf node-"$NODE_VERSION"-linux-"$ARCHSPECIFICVARIABLESHORT".tar.gz
+podman_exec /opt/ mv node-"$NODE_VERSION"-linux-"$ARCHSPECIFICVARIABLESHORT" node
+
+podman_exec /Signal-Desktop git-lfs install
+podman_exec /Signal-Desktop git config --global user.name name
+podman_exec /Signal-Desktop git config --global user.email name@example.com
+
+#podman_exec /Signal-Desktop sed -r '/mock/d' -i package.json
+
+podman_exec /Signal-Desktop npm install -g pnpm cross-env npm-run-all
+podman_exec /Signal-Desktop pnpm install
+podman_exec /Signal-Desktop rm -rf ts/test-mock
+podman_exec /Signal-Desktop pnpm run generate
+
+podman_exec /Signal-Desktop/sticker-creator pnpm install
+podman_exec /Signal-Desktop/sticker-creator pnpm run build
+
+podman_exec /Signal-Desktop pnpm run build:release --"$ARCHSPECIFICVARIABLESHORT" --linux
+
+# copy .deb out of builder container
 podman cp signal-desktop-"$VERSION":/Signal-Desktop/release/signal-desktop_"$VERSION"_"$ARCHSPECIFICVARIABLECOMMON".deb ~/signal-"$ARCHSPECIFICVARIABLECOMMON".deb
+
+# nice to have if we run on self-hosted infra, but since the vm is wiped every github actions run, this just adds extra time.
+# podman stop signal-desktop-"$VERSION"
+# podman rm signal-desktop-"$VERSION"
